82f6d3d6d3afb90b3e40b92ad0b26d1627e6e0b3
Nemanja Tonic <nemanjatonic@yahoo.com>
2016-10-12 22:43:27 +0200

    Checker/submit script demo

---
+++ octave/demo/submit_demo.m
@@ -0,0 +1,7 @@
+% Add path to submit script
+addpath('../')
+
+% Test if rgb2yuv.m is correct
+% NOTE: Fill in the empty parts of './out/rgb2yuv.m' to get a success message.
+% You can easily do this by copying the contents from './rgb2yuv.m'.
+submit ./out/rgb2yuv.m
